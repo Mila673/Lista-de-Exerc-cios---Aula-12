32c72c65a482aebc1aa2b86b4e7a9d5c82cda0b6
Mila673 <72633781+Mila673@users.noreply.github.com>
2024-07-30 16:49:30 -0300

    Main6.java

---
+++ Main6.java
@@ -0,0 +1,10 @@
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main6 {
+        public static void main(String[] args) {
+            Queue<Integer> fila = Util.criarFilaAleatoria(1000);
+            Stack<Integer> pilha = new Stack<>();
+            Util.processarFilaEPilha(fila, pilha);
+        }
+}
