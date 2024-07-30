9eff2a8458534958823afc10a9baf9c2888fc70e
Mila673 <72633781+Mila673@users.noreply.github.com>
2024-07-30 16:00:40 -0300

    Ex5.java

---
+++ Ex5.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.Random;
+
+class Ex5 {
+    private Queue<Integer> fila;
+    private Stack<Integer> pilha;
+    private Random gerador;
+
+    public Ex5() {
+        this.fila = new LinkedList<>();
+        this.pilha = new Stack<>();
+        this.gerador = new Random();
+    }
+
+    public void gerarNumeros(int quantidade) {
+        for (int i = 0; i < quantidade; i++) {
+            int numero = gerador.nextInt();
+            if (fila.contains(numero)) {
+                pilha.push(numero);
+            } else {
+                fila.add(numero);
+            }
+        }
+    }
+
+    public void imprimirEstruturas() {
+        System.out.println("Fila:");
+        while (!fila.isEmpty()) {
+            System.out.println(fila.poll());
+        }
+        System.out.println("Pilha:");
+        while (!pilha.isEmpty()) {
+            System.out.println(pilha.pop());
+        }
+    }
+}
