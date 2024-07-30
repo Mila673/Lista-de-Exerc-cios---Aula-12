ad8aa6c47591460ef27951b51647b4138a855b8a
Mila673 <72633781+Mila673@users.noreply.github.com>
2024-07-30 16:01:14 -0300

    Main5.java

---
+++ Main5.java
@@ -0,0 +1,7 @@
+public class Main5 {
+    public static void main(String[] args){
+        Ex5 gerador = new Ex5();
+        gerador.gerarNumeros(1000);
+        gerador.imprimirEstruturas();
+    }
+}
