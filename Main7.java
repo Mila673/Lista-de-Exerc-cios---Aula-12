16cbf72e3d8ed15d3f59c63cfee8aeca31ab097b
Mila673 <72633781+Mila673@users.noreply.github.com>
2024-07-30 17:52:26 -0300

    Main7.java

--- Main7.java
+++ Main7.java
@@ -5,14 +5,27 @@ public class Main7 {
     public static void main(String[] args) {
         Queue<Integer> fila = new LinkedList<>();
 
-        for (int i = 1; i <= 10; i++) {
-            fila.add(i);
-        }
+        fila.add(1);
+        fila.add(2);
+        fila.add(3);
+        fila.add(4);
+        fila.add(5);
+        fila.add(6);
+        fila.add(7);
+        fila.add(8);
+        fila.add(9);
+        fila.add(10);
+        fila.add(11);
+        fila.add(12);
+        fila.add(13);
+        fila.add(14);
+        fila.add(15);
+        fila.add(16);
+        fila.add(17);
+        fila.add(18);
+        fila.add(19);
+        fila.add(20);
 
-        System.out.println("Fila original: " + fila);
-        
-        Util.inverterFila(fila);
-
-        System.out.println("Fila invertida: " + fila);
+        Pilha.inverterEImprimir(fila);
     }
 }
